68848585526740f350ef42398920f9190cbb600a
shun360 <167048515+shun360@users.noreply.github.com>
2024-10-31 16:41:51 +0900

    [update] 一つのクイズデータを取得するAPI実装

--- python/getData.py
+++ python/getData.py
@@ -1,10 +1,26 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+def get_set_quiz(qid):
+    load_dotenv()
+
+    MONGO_USER = os.getenv("MONGO_USER")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+    MONGO_SERVER = os.getenv("MONGO_SERVER")
+    MONGO_DB = os.getenv("MONGO_DB")
+    mongo_connecter = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_SERVER}/"
+    try:
+        db = MongoClient(mongo_connecter)[MONGO_DB]
+        quiz = db['quizList'].find_one({"Qid": qid}, {'_id': 0})
+        return quiz
+    except Exception as e:
+        print('エラー：', e)
+        return None
 
 def get_all_quiz():
     load_dotenv()
@@ -20,13 +36,19 @@ def get_all_quiz():
         return all_quiz
     except Exception as e:
         print('エラー：', e)
-        return []
+        return None
 
 @app.get('/api/quizzes')
 def quizzes():
     quiz_data = get_all_quiz()
     return JSONResponse(content=quiz_data)
 
+@app.get('/api/quizzes/{qid}')
+def quiz_set(qid: int):
+    quiz_data = get_set_quiz(qid)
+    if quiz_data is not None:
+        return JSONResponse(content=quiz_data)
+    raise HTTPException(status_code=404, detail="Entry not found")
 
 if __name__ == '__main__':
     import uvicorn
